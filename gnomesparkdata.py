0b4f0a4026ba1de6522b52b56931333604c68473
MapTube <richard.milton@ucl.ac.uk>
2017-09-11 17:28:04 +0100

    added other gnomes talked to factor

--- gnomesparkdata.py
+++ gnomesparkdata.py
@@ -6,7 +6,8 @@ import csv
 
 
 def main():
-    infilename = "C:\\Users\\richard\\Desktop\\Gnomes\\SIGCHI\\gnome-data\\conversations.csv"
+    #infilename = "C:\\Users\\richard\\Desktop\\Gnomes\\SIGCHI\\gnome-data\\conversations.csv"
+    infilename = "C:\\Users\\richard\\Desktop\\SIGCHI\\gnomes-data\\20170911_conversations\\conversations_sep.csv"
     print("loading: ", infilename)
 
     creatureNames = [ 'Parker', 'Wombat', 'Loki', 'Gnomeo', 'Jetpack Gnomey', 'Super Gnome', 'Yusuf',
@@ -16,8 +17,20 @@ def main():
 
     #chatbot text following the user's yes or no after memory prompt
     #TODO: could also track "username YES|NO"
-    memoryTextYes = "Perfect! What would you like to tell me?"
+    memoryTextYes = [
+        "Perfect! What would you like to tell me?",
+        "Great! What would you like to tell me?",
+        "Great! What's your memory about?" ]
     memoryTextNo = "No problem " #plus name
+
+    #chatbot text when user meets a second gnome
+    otherGnomeText = [
+        "I see you've already spoken to ", #plus creature name
+        "You just spoke to ", #plus creature name
+        "I see you've just spoken to " #plus creature name
+    ]
+
+    #nextCreatureText = "You can find my buddy "
 
     #cID, INTERLOCUTOR, TEXT, LAT, LON
     #108, Duncan, hello, 51.54592, -0.01443
@@ -26,6 +39,7 @@ def main():
     namecount = {}
     memoryyescount = {}
     memorynocount = {}
+    othergnomecount = {}
     wordcount = {}
     with open(infilename, 'rt') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -43,8 +57,8 @@ def main():
                 namecount[name]=namecount[name]+1
             else:
                 namecount[name]=1
-            #count memories - either yes for memory left, or no for declined to leave one
-            if text==memoryTextYes:
+            #count memories - either yes for memory was left, or no for declined to leave one
+            if text in memoryTextYes:
                 if name in memoryyescount:
                     memoryyescount[name]=memoryyescount[name]+1
                 else:
@@ -54,6 +68,13 @@ def main():
                     memorynocount[name]=memorynocount[name]+1
                 else:
                     memorynocount[name]=1
+            #repeat counts when it detects you have visited another gnome before
+            for ogtext in otherGnomeText:
+                if text.startswith(ogtext):
+                    if name in othergnomecount:
+                        othergnomecount[name]=othergnomecount[name]+1
+                    else:
+                        othergnomecount[name]=1
             #word counts - but only from the visitors
             if not isGnomeText:
                 words = text.split()
@@ -69,17 +90,21 @@ def main():
 
 
     ###
-    #print("namecount")
+    print("name,count,memyes,memno,othergnomes")
     for key, value in namecount.items():
         memoryYes=0
         memoryNo=0
+        ogcount=0
         if key in memoryyescount:
             memoryYes=memoryyescount[key]
         if key in memorynocount:
             memoryNo=memorynocount[key]
-        print(key, ",", value, ",", memoryYes, ",", memoryNo)
+        if key in othergnomecount:
+            ogcount=othergnomecount[key]
+        print(key, ",", value, ",", memoryYes, ",", memoryNo, ",", ogcount)
 
     #word counts
+    print("")
     print("word counts")
     for key, value in wordcount.items():
         print(key, ",", value)
